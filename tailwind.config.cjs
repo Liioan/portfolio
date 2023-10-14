e28256f2bfe93791dd56e3e0a21ab4cce4cd280d
Liioan <dev.davidbijok@gmail.com>
2023-10-14 21:17:15 +0200

    add custom cursor, add fade in animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       colors: {
-        background: '#111111',
-        primary: '#26C9DF',
-        accent: '#1348B0',
-        text: '#F5F5F5',
+        background: "#111111",
+        primary: "#26C9DF",
+        accent: "#1348B0",
+        text: "#F5F5F5",
       },
-      background: 'linear-gradient(165deg, #20c4d9, #1348b0)',
+      background: "linear-gradient(165deg, #20c4d9, #1348b0)",
       fontFamily: {
-        heading: 'Montserrat',
-        content: 'Source Code Pro',
+        heading: "Montserrat",
+        content: "Source Code Pro",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 5s",
       },
     },
   },
