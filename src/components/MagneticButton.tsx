e30a85f0d29616a9c9c4197ce59395f8731c51cb
Liioan <dev.davidbijok@gmail.com>
2023-10-15 21:08:35 +0200

    disable magnetic buttons for mobile

--- src/components/MagneticButton.tsx
+++ src/components/MagneticButton.tsx
@@ -1,4 +1,4 @@
-import { useRef, type ReactNode, useEffect } from "react";
+import { useRef, type ReactNode, useEffect, useState } from "react";
 import { TweenMax, Power4 } from "gsap";
 
 interface MagneticButtonProps {
@@ -9,9 +9,24 @@ interface MagneticButtonProps {
 const MagneticButton = ({ className = "", children }: MagneticButtonProps) => {
   const buttonRef = useRef<any>();
 
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 640);
+
+  const onResize = () => {
+    setIsMobile(window.innerWidth < 640);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", onResize);
+
+    return () => {
+      window.removeEventListener("resize", onResize);
+    };
+  }, []);
+
   useEffect(() => {
     const strength = 50;
     buttonRef.current.addEventListener("mousemove", (e: MouseEvent) => {
+      if (isMobile) return;
       const magnetButton = buttonRef.current;
       const bounding = magnetButton?.getBoundingClientRect();
 
@@ -26,6 +41,7 @@ const MagneticButton = ({ className = "", children }: MagneticButtonProps) => {
       });
     });
     buttonRef.current.addEventListener("mouseout", (e: Event) => {
+      if (isMobile) return;
       TweenMax.to(e.currentTarget, 1, { x: 0, y: 0, ease: Power4.easeOut });
     });
   });
