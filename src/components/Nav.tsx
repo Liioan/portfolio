f09af5e1c0bc1a41b9cc9601afb9f6aa9e2d52fa
Liioan <dev.davidbijok@gmail.com>
2023-10-15 15:11:40 +0200

    add mobile nav button

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,71 @@
-import { useEffect, useState } from "react";
+import { ChevronDown, Menu } from "lucide-react";
+import { useEffect, useState, type SetStateAction, type Dispatch } from "react";
 import MagneticButton from "./MagneticButton";
+import { AnimatePresence, motion } from "framer-motion";
 
 const anchors = ["about", "toolbox", "projects", "contact"];
+
+const DesktopNav = () => {
+  return (
+    <ul className="flex w-min flex-col justify-end">
+      {anchors.map((anchor) => (
+        <li
+          key={anchor}
+          className="hide-cursor flex w-full justify-end text-[32px] font-semibold text-white opacity-50 transition-opacity duration-200 hover:opacity-100"
+        >
+          <MagneticButton>
+            <a href={`#${anchor}`}>{anchor}</a>
+          </MagneticButton>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+interface NavButtonProps {
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+const NavButton = ({ isOpen, setIsOpen }: NavButtonProps) => {
+  return (
+    <button
+      onClick={() => setIsOpen((prev) => !prev)}
+      className="relative h-12 w-12"
+    >
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            className="absolute left-0 top-0"
+            initial={{ y: -10, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: -10, opacity: 0 }}
+          >
+            <ChevronDown size={48} color="white" />
+          </motion.div>
+        )}
+      </AnimatePresence>
+      <AnimatePresence>
+        <motion.div
+          className="absolute left-0 top-0"
+          animate={{
+            y: isOpen ? -10 : 0,
+            opacity: isOpen ? 0.5 : 1,
+            scale: isOpen ? 0.5 : 1,
+          }}
+        >
+          <Menu size={48} color="white" />
+        </motion.div>
+      </AnimatePresence>
+    </button>
+  );
+};
+
+const MobileNav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return <NavButton isOpen={isOpen} setIsOpen={setIsOpen} />;
+};
 
 const Nav = () => {
   const [isMobile, setIsMobile] = useState(window.innerWidth < 640);
@@ -21,15 +85,7 @@ const Nav = () => {
   return (
     <nav className="fixed left-0 top-0 z-20 flex w-full justify-between p-[30px]">
       <img src="/logo.png" alt="" className="h-[67px] w-[67px]" />
-      <ul className="flex w-min flex-col justify-end">
-        {anchors.map((anchor) => (
-          <li className="hide-cursor flex w-full justify-end text-[32px] font-semibold text-white opacity-50 transition-opacity duration-200 hover:opacity-100">
-            <MagneticButton>
-              <a href={`#${anchor}`}>{anchor}</a>
-            </MagneticButton>
-          </li>
-        ))}
-      </ul>
+      {isMobile ? <MobileNav /> : <DesktopNav />}
     </nav>
   );
 };
